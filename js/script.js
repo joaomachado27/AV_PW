fa912e98dc9de71755c3f9a8667a96e499dd26b9
João Vitor Machado <jvjumachado@gmail.com>
2024-05-17 11:07:28 -0300

    9° commit - Alterando Página Curso e Protótipo Página Form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+document.querySelector('input[type=email').oninvalid = function(){
+    // remove mensagens de erro antigas
+    this.setCustomValidity("");
+
+    // reexecuta a validação
+    if(!this.validity.valid){
+
+        // se inválido, coloca mensagem de erro
+        this.setCustomValidity("Email inválido");
+    }
+};
+
+/*document.querySelector('#form-busca').onsubmit = function(){
+    if (document.querySelector('#q').value == ''){
+        document.querySelector('#q').style.border = '1px solid red';
+        return false;
+    }
+}
+*/
